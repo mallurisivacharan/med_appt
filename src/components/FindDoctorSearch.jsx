999881b6a088bc20be09a626fa8ceffb51d72d10
mallurisivacharan <mallurisivacharan@gmail.com>
2026-05-09 14:47:36 +0530

    Update FindDoctorSearch.jsx

--- src/components/FindDoctorSearch.jsx
+++ src/components/FindDoctorSearch.jsx
@@ -1,20 +1,33 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function FindDoctorSearch() {
+  const [specialty, setSpecialty] = useState("");
 
-  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    if (specialty !== "") {
+      navigate(`/search/${specialty}`);
+    }
+  };
 
   return (
     <div>
-      <h2>Find Doctor</h2>
+      <h2>Find Doctors</h2>
 
-      <input
-        type="text"
-        placeholder="Search doctor"
-        onChange={(e) => setSearch(e.target.value)}
-      />
+      <select
+        value={specialty}
+        onChange={(e) => setSpecialty(e.target.value)}
+      >
+        <option value="">Select Specialty</option>
+        <option value="Cardiologist">Cardiologist</option>
+        <option value="Dermatologist">Dermatologist</option>
+        <option value="Neurologist">Neurologist</option>
+        <option value="Orthopedic">Orthopedic</option>
+      </select>
 
-      <p>Searching for: {search}</p>
+      <button onClick={handleSearch}>Search</button>
     </div>
   );
 }
